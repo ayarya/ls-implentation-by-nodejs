c835137c17c8405ff50ea2ce4f10db2a924f840c
Ayush Arya <arya.ayush409@gmail.com>
2020-05-17 22:49:57 -0400

    folder is printed bold and file is printed not bold with chalk

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const chalk = require('chalk');
 
-fs.readdir(process.cwd(), (err, filenames) => {
+const { lstat } = fs.promises;
+
+fs.readdir(process.cwd(), async (err, filenames) => {
 	if (err) {
 		console.log(err);
 	}
-	console.log(filenames);
+	const statPromises = filenames.map((filename) => {
+		return lstat(filename);
+	});
+
+	const allStats = await Promise.all(statPromises);
+	for (let stats of allStats) {
+		const index = allStats.indexOf(stats);
+		if (stats.isFile()) {
+			console.log(filenames[index]);
+		} else {
+			console.log(chalk.bold(filenames[index]));
+		}
+	}
 });
